c5d73982cfa576390461a86f22dccf47fd72a4b3
Belkis Asma SEMCHEDDINE <semcheddineasma@yahoo.fr>
2022-03-10 19:24:49 +0100

    fixed images bug inwsgi.py

--- conf/wsgi.py
+++ conf/wsgi.py
@@ -16,4 +16,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'conf.settings')
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'build/static'))
+application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'build'))
